27863569a96d905fc8c52dfa0e6d35d09f1ae414
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-06-27 16:19:44 -0500

    Small refactor, including header, imports and some pep8 warnings

--- src/caliope_server.py
+++ src/caliope_server.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 '''
-Created on 27/06/2013
-
 @authors: Andrés Felipe Calderón andres.calderon@correlibre.org
           Sebastián Ortiz neoecos@gmail.com
 
 @license:  GNU AFFERO GENERAL PUBLIC LICENSE
 
-Caliope Storage is the base of Caliope's Framework
+Caliope Server is the web server of Caliope's Framework
 Copyright (C) 2013 Fundación Correlibre
 
     This program is free software: you can redistribute it and/or modify
@@ -24,18 +22,22 @@ Copyright (C) 2013 Fundación Correlibre
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+#system, and standard library
 import os
 import getopt
 import sys
 import json
 
+#gevent
 from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
+from gevent import monkey
+
+#flask
 from flask import Flask, render_template, send_from_directory
-from jinja2 import FileSystemLoader 
+from jinja2 import FileSystemLoader
 
-from gevent import monkey;
-
+#Blueprints
 from api.views import api
 from server_notifications.views import server_notifications
 from file_uploader.views import file_uploader
@@ -56,32 +58,32 @@ app.register_blueprint(file_uploader, url_prefix='/upload')
 @app.route('/')
 def index():
         return render_template('index.html')
-    
+
+
 @app.route('/<path:filename>')
 def custom_static(filename):
     return send_from_directory(app.config['STATIC_PATH'], filename)
 
-def main(argv): 
+
+def main(argv):
     configfile = "conf/caliope_server.json"
     try:
         opts, args = getopt.getopt(argv, "hc:", ["help", "config="])
     except getopt.GetoptError:
-      print 'runserver.py -c <configfile>'
-      sys.exit(2)
-      
+        print 'caliope_server.py -c <configfile>'
+        sys.exit(2)
+
     for opt, arg in opts:
         if opt == '-h':
-            print 'runserver.py -c <configfile>'
+            print 'caliope_server.py -c <configfile>'
             sys.exit()
         elif opt in ("-c", "--config"):
             configfile = arg
-        
     try:
         config = json.loads(open(configfile).read())
     except IOError:
         print "Error: can\'t find config file or read data"
     else:
-        
         if 'port' in config:
             port = int(config['port'])
         else:
@@ -91,13 +93,13 @@ def main(argv):
             app.config['STATIC_PATH'] = config['static']
         else:
             app.config['STATIC_PATH'] = "."
-        
         print "=============================="
         print "listening at port : " + str(port)
         print "static base directory : " + app.config['STATIC_PATH']
         print "=============================="
 
-        app.jinja_loader = FileSystemLoader(os.path.join(".", app.config['STATIC_PATH'])) 
+        app.jinja_loader = FileSystemLoader(os.path.join(".",
+                                            app.config['STATIC_PATH']))
         http_server = WSGIServer(('', port), app, handler_class=WebSocketHandler)
         http_server.serve_forever()
 
