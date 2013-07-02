bae6928d68a140f6108038fa07dd65c4c3ed2167
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-07-02 18:13:33 -0500

    Added util for load JSON from files

---
+++ src/cid/utils/fileUtils.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+'''
+@authors: Sebastián Ortiz V. neoecos@gmail.com
+
+@license:  GNU AFFERO GENERAL PUBLIC LICENSE
+
+Caliope Server is the web server of Caliope's Framework
+Copyright (C) 2013 Infometrika
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+#system, and standard library
+import os
+import sys
+import json
+
+
+#: TODO: Pending to  implement
+def loadJSONFromFile(path):
+    try:
+        #: TODO use utils.readJSONFromFile
+        json_data = json.loads(open(path).read())
+    except IOError:
+        json_data = {}
+        print "Error: can\'t find file or read data"
+    except ValueError:
+        json_data = {}
+        print "Error, is not a JSON" + path
+    else:
+        return json_data
