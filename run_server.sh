b76801368c74ba3df6d1b9606cd500e26ee9891c
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-07-18 00:32:40 -0500

    Added correct PYTHONPATH

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 BASE=$(echo $PWD | awk -F'caliope_server_el_cid' '{print $1}')
-
+export PYTHONPATH=${BASE}/caliope_server_el_cid/src
 cd src/cid
 ./caliope_server.py -c conf/caliope_server.json
