cefca8110de61898c3b4950c5705d982c59e10d9
Andres Calderon <andres.calderon@correlibre.org>
2013-07-30 17:40:23 -0500

    document managment added

---
+++ src/cid/core/documents/__init__.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+"""
+@authors: Andrés Felipe Calderón andres.calderon@correlibre.org
+          Sebastián Ortiz V. neoecos@gmail.com
+
+SIIM2 Server is the web server of SIIM2 Framework
+Copyright (C) 2013 Infometrika Ltda.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+#CaliopeStorage
+from neomodel import DoesNotExist
+from odisea.CaliopeStorage import CaliopeUser
+
+#tinyrpc
+from tinyrpc.protocols.jsonrpc import JSONRPCInvalidRequestError, JSONRPCInternalError
+from tinyrpc.dispatch import public
+
+#Flask
+from flask import current_app, g
+
+import documentmodel
+
+class DocumentManager(object):
+    @staticmethod
+    @public
+    def getAll():
+        return JSONRPCInternalError('Unimplemented')
+    
+    @staticmethod
+    @public
+    def getFilteredByProyect(proyect_id):
+        return JSONRPCInternalError('Unimplemented')
+    
+    @staticmethod
+    @public
+    def setState():
+        return JSONRPCInternalError('Unimplemented')
+
+    @staticmethod
+    def addDocument(parent_uuid, url, description):
+        node = DocumentNode()
+        node.add_to_repo(parent_uuid, url, description)
+        
+        
