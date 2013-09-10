2ee29a124125e5cfa622d56cdba65de4078aa632
Andres Calderon <andresfcalderon@gmail.com>
2013-09-10 11:12:59 -0500

    dummy get_thumbnail bug fixed

--- src/cid/core/accounts/services.py
+++ src/cid/core/accounts/services.py
@@ -19,7 +19,7 @@ Copyright (C) 2013 Infometrika Ltda.
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
+import os
 #
 from neomodel import DoesNotExist
 
