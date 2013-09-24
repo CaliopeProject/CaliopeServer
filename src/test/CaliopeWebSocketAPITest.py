04fe1d9548a7c21146deb695df772cdaf4887902
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-09-24 13:42:38 -0500

    Added unittest for server with websocket

---
+++ src/test/CaliopeWebSocketAPITest.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@authors: Sebastián Ortiz V. neoecos@gmail.com
+
+SIIM Server is the web server of SIIM's Framework
+Copyright (C) 2013 Infometrika Ltda
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import unittest
+import json
+import uuid
+import hashlib
+import time
+#gevent
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+from gevent import monkey
+
+#tinyrpc
+from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
+from tinyrpc.client import RPCClient
+from tinyrpc.transports.http import HttpWebSocketClientTransport
+
+#flask
+from flask import (Flask)
+from flask.helpers import safe_join
+
+#simplekv
+import redis
+from simplekv.memory.redisstore import RedisStore
+
+from cid import caliope_server
+
+
+class CaliopeServerTestUnit(unittest.TestCase):
+    def setUp(self):
+        caliope_server.app.config['TESTING'] = True
+        caliope_server.init_flask_app()
+        caliope_server.configure_server_and_app("../../conf/caliope_server.json")
+        caliope_server.configure_logger("../../conf/tests_logger.json")
+        caliope_server.register_modules()
+        caliope_server.app.storekv = RedisStore(redis.StrictRedis())
+        self.http_server = WSGIServer((caliope_server.app.config['test_address'],
+                                       caliope_server.app.config['test_port']),
+                                      caliope_server.app,
+                                      handler_class=WebSocketHandler)  # @IgnorePep8
+        self.http_server.start()
+
+    def tearDown(self):
+        """Get rid of the database again after each test."""
+        if self.rpc_client:
+            self.rpc_client.transport.close()
+        self.http_server.stop()
+        self.http_server = None
+        caliope_server.app = Flask('caliope_server')
+        time.sleep(1)
+
+
+    def login(self, username, password):
+        self.rpc_client = RPCClient(JSONRPCProtocol(),
+                                    HttpWebSocketClientTransport('ws://localhost:9001/api/ws'))
+        self.loginManager = self.rpc_client.get_proxy("login.")
+        hashed_password = hashlib.sha256(password).hexdigest()
+        return self.loginManager.authenticate(username=username,
+                                              password=hashed_password)
+
+    def logout(self, uuid):
+        if self.loginManager is None:
+            return
+        return self.loginManager.logout(uuid=uuid)
+
+
+    def test_login(self):
+        rv = self.login(u'user', u'123')
+        assert 'login' in rv
+        assert rv['login'] is True
+        assert 'uuid' in rv
+
+    def test_logout(self):
+        uuid = self.login(u'user', u'123')['uuid']
+        rv = self.logout(uuid=uuid)
+        assert 'logout' in rv
+        assert rv['logout'] is True
+
+
+if __name__ == '__main__':
+    unittest.main()
