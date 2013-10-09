df2fade9f71d7c05b0efa9547c161f05a8119a73
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-10-09 15:07:48 -0500

    Added some reconnect test

--- src/cid/core/entities/base_models/versioned_node.py
+++ src/cid/core/entities/base_models/versioned_node.py
@@ -25,10 +25,11 @@ Copyright (C) 2013  Fundación Correlibre
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from neomodel import StringProperty, DateTimeProperty, RelationshipTo, ZeroOrOne, DoesNotExist, RelationshipDefinition, RelationshipManager
+from neomodel import StringProperty, DateTimeProperty, RelationshipTo, ZeroOrOne, One, DoesNotExist, RelationshipDefinition, RelationshipManager, RelationshipFrom
 from neomodel.contrib import SemiStructuredNode
 
 from cid.core.utils import uuidGenerator, timeStampGenerator
+
 
 class VersionedNode(SemiStructuredNode):
     """
@@ -37,14 +38,14 @@ class VersionedNode(SemiStructuredNode):
 
     __index__ = 'CaliopeStorage'
 
-    uuid = StringProperty(default = uuidGenerator, unique_index = True)
+    uuid = StringProperty(default=uuidGenerator, unique_index=True)
 
     #: All timestamps should be in UTC using pytz.utc
     # TODO:
     # 1) When a timestamp is stored and then loaded the value is different.
     #    Timezone issue.
     # 2) Check that the timestamp is updated when needed.
-    timestamp = DateTimeProperty(default = timeStampGenerator)
+    timestamp = DateTimeProperty(default=timeStampGenerator)
 
     __special_fields__ = set(['timestamp', 'parent', 'uuid'])
 
@@ -52,114 +53,107 @@ class VersionedNode(SemiStructuredNode):
         cls.parent = RelationshipTo(cls, 'PARENT', ZeroOrOne)
         return super(VersionedNode, cls).__new__(cls, *args, **kwargs)
 
+    def __init__(self, *args, **kwargs):
+        super(VersionedNode, self).__init__(*args, **kwargs)
+
     def _attributes_to_diff(self):
         return [a for a in self.__dict__ if a[:1] != '_' \
-                                         and a not in self.__special_fields__]
+            and a not in self.__special_fields__]
 
     def _should_save_history(self, stored_node):
         for field in set(self._attributes_to_diff() +
-                         stored_node._attributes_to_diff()):
-            # If versioned nodes have different fields they are different.
-           if not hasattr(stored_node, field) or not hasattr(self, field):
-               return True
-           # A field has a different value.
-	   if getattr(self, field) != getattr(stored_node, field):
-               return True
-        # Versioned nodes have the save fields and field values.
+                stored_node._attributes_to_diff()):
+        # If versioned nodes have different fields they are different.
+            if not hasattr(stored_node, field) or not hasattr(self, field):
+                return True
+                # A field has a different value.
+            if getattr(self, field) != getattr(stored_node, field):
+                return True
+                # Versioned nodes have the save fields and field values.
         return False
 
-    """def _copy_connections_broken(self, new_node):
-        for attribute_name in self._attributes_to_diff():
-            if not issubclass(getattr(self, attribute_name).__class__, \
-                              RelationshipManager):
-                continue # Not a relationship. Nothing to do.
-	    if not hasattr(new_node, attribute_name):
-                continue # New node doesn't have the relationship.
-	    self_rel = getattr(self, attribute_name)
-	    new_rel = getattr(new_node, attribute_name)
-            print 'hey', new_rel.__class__
-	    for node in self_rel.all():
-                print 'old rel', self_rel
-                print 'BEGIN connecting', new_rel, 'to', node
-                print 'New node uuid', new_node.uuid
-	        new_rel.connect(node)
-                print 'END'
-    """
+    #def _relationships_to_diff(self):
+
+
 
     @classmethod
-    def _copy_connections(cls, old_node, new_node):
+    def _copy_relationships(cls, old_node, new_node):
         for key, val in cls.__dict__.items():
             if issubclass(val.__class__, RelationshipDefinition):
                 if key != 'parent' and hasattr(new_node, key):
                     new_rel = getattr(new_node, key)
                     old_rel = getattr(old_node, key)
-                    print key, val.__class__
-                    print 'old_rel', old_rel
-                    print 'new_rel', new_rel
-                    for n in old_rel.all():
-                        #saved = False
-                        #for node_class_rel in n._get_class_relationships():
-                        #    node_rel = getattr(n,node_class_rel[0], None)
-                        #    if issubclass(node_rel.__class__, (One, ZeroOrOne)):
-                        #        if (getattr(node_rel, 'relation_type', None) ==
-                        #            getattr(new_rel, 'relation_type', None)):
-                        #            node_rel.reconnect(old_node, new_node)
-                        #            saved = True
-                        #if not saved:
-                        #    new_rel.connect(n)
-                        new_rel.connect(n)
+                    for related_node in old_rel.all():
+                        saved = False
+                        for node_class_rel in related_node._get_class_relationships():
+                            node_rel = getattr(related_node, node_class_rel[0], None)
+                            if issubclass(node_rel.__class__, (One, ZeroOrOne)):
+                                if (getattr(node_rel, 'relation_type') ==
+                                        getattr(new_rel, 'relation_type')):
+                                    node_rel.reconnect(old_node, new_node)
+                                    saved = True
+                        if not saved:
+                            new_rel.connect(related_node)
 
-    def save(self, skip_difference = False):
+    def save(self, skip_difference=False):
         if not skip_difference:
             # TODO(nel): Don't use an exception here.
             try:
-                stored_node = self.__class__.index.get(uuid = self.uuid)
+                stored_node = self.__class__.index.get(uuid=self.uuid)
             except DoesNotExist:
                 stored_node = None
             if stored_node and self._should_save_history(stored_node):
                 # The following operations should be atomic.
                 copy = stored_node.__class__()
-                print 'class', stored_node.__class__
-                print 'COPY', copy
-                print 'STOR', stored_node
-	        for field in stored_node._attributes_to_diff():
-                    setattr(copy, field, getattr(stored_node, field))
-		copy.save(skip_difference = True)
+                for field in stored_node._attributes_to_diff():
+                    if not isinstance(getattr(stored_node, field), RelationshipManager):
+                        setattr(copy, field, getattr(stored_node, field))
+                copy.save(skip_difference=True)
                 if len(self.parent):
                     copy.parent.connect(self.parent.get())
                     self.parent.disconnect(self.parent.get())
                 self.parent.connect(copy)
-                #self._copy_connections(self, copy)
+                self._copy_relationships(self, copy)
                 self.timestamp = timeStampGenerator()
         super(VersionedNode, self).save()
 
-    def __init__(self, *args, **kwargs):
-        super(VersionedNode, self).__init__(*args, **kwargs)
+    @classmethod
+    def _get_class_relationships(cls):
+        return [(rel, rel_inst)
+                for rel, rel_inst in cls.__dict__.items()
+                if rel and isinstance(rel_inst, RelationshipDefinition)]
 
-#class Car(VersionedNode):
-#  plate = StringProperty()
 
-# TODO(nel): Make this a test.
-# Sample use:
-#class Person(VersionedNode):
-#   name = StringProperty()
-#   age = StringProperty()
-#   car = RelationshipTo(Car, 'CAR')
+class Person(object):
+    pass
 
-#person = Person(name = 'Alice')
-#person.age = 10
-#person.save()
-#person = Person(name = 'Bob')
-#person.age = 10
 
-#car = Car(plate = '7777')
-#car.save()
-#person.save()
-#person.car.connect(car)
+class Car(VersionedNode):
+    plate = StringProperty()
+    owner = RelationshipFrom(Person, 'CAR', ZeroOrOne)
 
-#person.age = 20
-#person.save()
-#person.age = 30
-#person.save()
-#person.age = 40
-#person.save()
+
+class Person(VersionedNode):
+    name = StringProperty()
+    age = StringProperty()
+    car = RelationshipTo(Car, 'CAR')
+
+
+person = Person(name='Alice')
+person.age = 10
+person.save()
+person = Person(name='Bob')
+person.age = 10
+
+car = Car(plate='7777')
+car.save()
+person.save()
+person.car.connect(car)
+
+person.age = 20
+person.save()
+person.age = 30
+person.save()
+person.age = 40
+person.save()
+print person.__node__
