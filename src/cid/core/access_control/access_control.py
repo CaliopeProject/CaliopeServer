f7dc6d6d26b2778293e307327c01e935dc8423a7
Nelson Castillo <nelsoneci@gmail.com>
2013-11-01 18:04:37 -0500

    Lista la detección de referencias cicrulares.

--- src/cid/core/access_control/access_control.py
+++ src/cid/core/access_control/access_control.py
@@ -6,15 +6,18 @@ class AccessControl:
   def __init__(self, configuration):
       self.config = json.loads(configuration)
 
-  def ResolveGroups(self, group_name, seen=set()):
+  def _ResolveGroups(self, group_name, seen):
       if group_name in seen:
-        print >> sys.stderr, 'circular reference found for group {}'.format(group_name)
+        print >> sys.stderr, 'Circular reference found. Group:{}'.format(group_name)
+        sys.exit(1)
       seen.add(group_name)
       members = []
       for member_or_group in self.config['groups'][group_name]:
-          if member_or_group in self.groups:
-              members += self.ResolveGroups(member_or_group, seen)
+          if member_or_group in self.config['groups']:
+              # A group. Let's recourse.
+              members += self._ResolveGroups(member_or_group, seen)
           else:
+              # A member. Let's add it.
               members.append(member_or_group)
       return members
 
@@ -27,16 +30,8 @@ class AccessControl:
       # First load the groups. Groups names have priority over user names.
       self.groups = {}
       for group in self.config['groups']:
-          print 'resolving group {}'.format(group)
-          self.groups[group] = self.ResolveGroups(group)
-
-      for group in self.config['groups']:
-        print 'group({}) => {}'.format(group, self.groups[group])
-      #    for user in self.config['groups'][group]:
-      #        #if user in self.config['groups']:
-      #        #    print >> sys.stderr, 'Username {} has been used already as group name'.format(user)
-      #        #    sys.exit(1)
-      #        pass
+          # Now get the users for this group.
+          self.groups[group] = self._ResolveGroups(group, seen=set())
 
 def main():
     ac = AccessControl(open('permissions.json').read())
