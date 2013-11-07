1cb268be582da8068fe24ae7da7ecc562027c2b4
Nelson Castillo <nelsoneci@gmail.com>
2013-11-06 20:02:07 -0500

    Small refactor.

--- src/cid/core/access_control/access_control.py
+++ src/cid/core/access_control/access_control.py
@@ -8,21 +8,25 @@ class AccessControl:
         """ Initialize the access control instance. """
         self.config = json.loads(configuration)
 
-    def _resolve_groups(self, group_name, seen):
-        """ Get the users that belong to a group. """
-        if group_name in seen:
-          print >> sys.stderr, 'Circular reference found. Group:{}'.format(group_name)
+    def _resolve_right_side(self, config_kind, key_name, seen):
+        """ Resolve right side in configuration. Useful for rules that can use
+            other rules. For instance, a group can specify other groups in the
+            configuration. Circular references are detected.
+            config_kind can be: groups, things, actions.
+        """
+        if key_name in seen:
+          print >> sys.stderr, 'Circular reference found resolving {} : {}'.format(config_kind, key_name)
           sys.exit(1)
-        seen.add(group_name)
-        members = []
-        for member_or_group in self.config['groups'][group_name]:
-            if member_or_group in self.config['groups']:
-                # A group. Let's recourse.
-                members += self._resolve_groups(member_or_group, seen)
+        seen.add(key_name)
+        right_side = []
+        for instance_or_kind in self.config[config_kind][key_name]:
+            if instance_or_kind in self.config[config_kind]:
+                # A group of things(kind). Let's recourse to resolve.
+                right_side += self._resolve_right_side(config_kind, instance_or_kind, seen)
             else:
-                # A member. Let's add it.
-                members.append(member_or_group)
-        return members
+                # An instance. Let's add it.
+                right_side.append(instance_or_kind)
+        return right_side
 
     def load_groups_and_users(self):
         """ Populate groups with their users. Groups can belong to other groups. """
@@ -33,18 +37,21 @@ class AccessControl:
         self.groups = {}
         for group in self.config['groups']:
             # Now get the users for this group.
-            self.groups[group] = self._resolve_groups(group, seen=set())
+            self.groups[group] = self._resolve_right_side('groups', group, seen=set())
 
     def load_things(self):
-        """ Get the list of things that can be used. """
-        available_things = things.get_available_things()
+        """ Load the list of things that we can use. """
+        # Make sure we do not call this function twice.
+        assert not hasattr(self, 'things')
+
         self.things = {}
-        for thing in self.config['things']:
-            print thing
-            assert isinstance(self.config['things'][thing], list)
-          #if thing not in available_things:
-          #  print >> sys.stderr, '{} is not in the list of available things. Available things: {}'.format(thing, available_things.keys())
-          #  sys.exit(1)
+        for thing_left in self.config['things']:
+            self.things[thing_left] = self._resolve_right_side('things', thing_left, seen=set())
+        # Check that all the things can be resolved to a class we know.
+        self.available_things = things.get_available_things()
+        for thing_left in self.config['things']:
+            for right_thing in self.things[thing_left]:
+                print  right_thing
 
 
     def get_groups(self):
