c2d72f1214a9990f5ba096afb1411d0971fc2b46
Nelson Castillo <nelsoneci@gmail.com>
2013-11-07 15:36:06 -0500

    Added actions.

--- src/cid/core/access_control/actions.py
+++ src/cid/core/access_control/actions.py
@@ -1,11 +1,23 @@
-class Actions:
-    pass
+class Action:
+    def __init__(self, name):
+        self.name = name
 
-class Read(Actions):
-    pass
+class Read(Action):
+    def __init__(self):
+        Action.__init__(self, "read")
 
-class Write(Actions):
-    pass
+class Write(Action):
+    def __init__(self):
+        Action.__init__(self, "write")
 
-class Assign(Actions):
-    pass
+class Assign(Action):
+    def __init__(self):
+        Action.__init__(self, "assign")
+
+def get_available_actions():
+    actions = [
+                Read(),
+                Write(),
+                Assign(),
+              ]
+    return {t.name: t for t in actions}
