f42912e84fe8314a4acd52dfee9a6c9d6cf9b54d
Nelson Castillo <nelsoneci@gmail.com>
2013-11-13 15:47:18 -0500

    Improved. Now the test works. This need more work.

--- src/cid/core/access_control/services.py
+++ src/cid/core/access_control/services.py
@@ -6,6 +6,7 @@ from cid.utils.fileUtils import loadJSONFromFile
 from flask.globals import current_app
 
 import access_control
+import os
 
 class AccessControlManager:
     @staticmethod
@@ -13,29 +14,19 @@ class AccessControlManager:
         """ Instanciate the access control object and return it.
             Read the current ACL configuration.
         """
-"""
-        # TODO(nel): Move from a file to another kind of configuration.
-        acl_path = os.path.join(current_app.config['cid_base_directory'],
-                                'conf',
-                                current_app.config['server']['acl_file'])
-        conf = loadJSONFromFile(current_app.config['server']['acl_file'] , 'conf' )
-        with open(acl_path) as acl_file:
-            return access_control.AccessControl(acl_file.read())
-            """
-
-    @staticmethod
-    @public(name='isAccessGranted')
-    def list_groups(params):
-        #TODO(nel): Get rid of the parameters.
-        acl = AccessControlManager.get_acl()
-
+        acl_conf = loadJSONFromFile(current_app.config['server']['acl_file'],
+                                    os.path.join(current_app.root_path, '../../'))
+        return access_control.AccessControl(acl_conf)
 
     @staticmethod
     @public(name='isAccessGranted')
     def is_access_granted(params):
-        # Unused.
+
+        # ***************************** Unused ****************************
         acl = AccessControlManager.get_acl()
+        # ***************************** Unused ****************************
+
         rv = {
-            'granted': True,
+            'granted': True
         }
         return rv
