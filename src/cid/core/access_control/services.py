519fd7af242ae4dd9295acf790c53f80911bd8b8
Nelson Castillo <nelsoneci@gmail.com>
2013-11-12 19:07:01 -0500

    This will allow to have settings for access control.

--- src/cid/core/access_control/services.py
+++ src/cid/core/access_control/services.py
@@ -1,14 +1,18 @@
-__author__ = 'afc'
-
 #tinyrpc
 from tinyrpc.protocols.jsonrpc import JSONRPCInvalidRequestError
 from tinyrpc.dispatch import public
+
+from flask.globals import current_app
+
+import json
+
 
 class AccessControlManager:
     @staticmethod
     @public(name='isAccessGranted')
     def is_access_granted(params):
         rv = {
-            "granted": True
+            'granted': True,
+            'test' : str(current_app.config['server']['acl_file'])
         }
         return rv
