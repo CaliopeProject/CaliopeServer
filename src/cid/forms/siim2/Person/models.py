5c2b082763f9642d9bdbd6f22bd001a69e43290e
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-12-12 15:28:16 -0500

    Added support for index on Person.name

--- src/cid/forms/siim2/Person/models.py
+++ src/cid/forms/siim2/Person/models.py
@@ -7,9 +7,10 @@ from cid.core.entities.base_models.versioned_node import VersionedNode
 
 
 class Person(VersionedNode):
-    name = StringProperty()
+    name = StringProperty(index=True, index_name="PersonNames",
+                          index_config={"type" : "fulltext"})
     surname = StringProperty()
     surname1 = StringProperty()
-    genero = StringProperty()
+    gender = StringProperty()
     address = RelationshipTo(VersionedNode, 'IS_IN', cardinality=ZeroOrMore)
 
